91de1fa503767694edc9ff4934b8c95da56a4ed4
tranthe170 <2121013745@sv.ufm.edu.vn>
2024-05-14 15:13:26 +0700

    page view schema

--- spark_streaming/schema.py
+++ spark_streaming/schema.py
@@ -22,3 +22,29 @@ listen_events_schema = StructType([
     StructField("gender", StringType(), True),
     StructField("registration", LongType(), True)
 ])
+
+
+page_view_events_schema = StructType([
+    StructField("ts", LongType(), True),
+    StructField("sessionId", IntegerType(), True),
+    StructField("page", StringType(), True),
+    StructField("auth", StringType(), True),
+    StructField("method", StringType(), True),
+    StructField("status", IntegerType(), True),
+    StructField("level", StringType(), True),
+    StructField("itemInSession", IntegerType(), True),
+    StructField("city", StringType(), True),
+    StructField("zip", IntegerType(), True),
+    StructField("state", StringType(), True),
+    StructField("userAgent", StringType(), True),
+    StructField("lon", DecimalType(), True),
+    StructField("lat", DecimalType(), True),
+    StructField("userId", IntegerType(), True),
+    StructField("lastName", StringType(), True),
+    StructField("firstName", StringType(), True),
+    StructField("gender", StringType(), True),
+    StructField("registration", LongType(), True),
+    StructField("artist", StringType(), True),
+    StructField("song", StringType(), True),
+    StructField("duration", DecimalType(), True)
+])
