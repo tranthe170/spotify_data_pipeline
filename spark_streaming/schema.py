eb5c9883e9ca7b04d3faf74de241c9692cd066ec
tranthe170 <2121013745@sv.ufm.edu.vn>
2024-05-14 14:24:44 +0700

    listen events stream

---
+++ spark_streaming/schema.py
@@ -0,0 +1,24 @@
+from pyspark.sql.types import (DecimalType, IntegerType, LongType, StringType,
+                               StructField, StructType, TimestampType)
+
+listen_events_schema = StructType([
+    StructField("artist", StringType(), True),
+    StructField("song", StringType(), True),
+    StructField("duration", DecimalType(), True),
+    StructField("ts", LongType(), True),
+    StructField("sessionid", IntegerType(), True),
+    StructField("auth", StringType(), True),
+    StructField("level", StringType(), True),
+    StructField("itemInSession", IntegerType(), True),
+    StructField("city", StringType(), True),
+    StructField("zip", IntegerType(), True),
+    StructField("state", StringType(), True),
+    StructField("userAgent", StringType(), True),
+    StructField("lon", DecimalType(), True),
+    StructField("lat", DecimalType(), True),
+    StructField("userId", LongType(), True),
+    StructField("lastName", StringType(), True),
+    StructField("firstName", StringType(), True),
+    StructField("gender", StringType(), True),
+    StructField("registration", LongType(), True)
+])
