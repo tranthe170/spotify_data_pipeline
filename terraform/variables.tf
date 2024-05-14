b30dedae7fa5bc52a35a369c2fd4c0500b022590
tranthe170 <2121013745@sv.ufm.edu.vn>
2024-05-14 15:31:42 +0700

    spark cluster, gcs bucket

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,24 @@
-locals {
-  data_lake_bucket = "streamify_data_lake"
-}
-
 variable "project" {
   description = "Your GCP Project ID"
   default     = "spotify-data-pipeline"
+  type        = string
+}
+
+variable "storage_class" {
+  description = "Storage class type for your bucket"
+  default     = "STANDARD"
+  type        = string
+}
+
+variable "vm_image" {
+  description = "Image for you VM"
+  default     = "ubuntu-os-cloud/ubuntu-2004-lts"
+  type        = string
+}
+
+variable "network" {
+  description = "Network for your instance/cluster"
+  default     = "default"
   type        = string
 }
 
@@ -18,9 +32,4 @@ variable "zone" {
   description = "Your project zone"
   default     = "us-central1-a"
   type        = string
-}
-
-variable "storage_class" {
-  description = "Storage class type for your bucket"
-  default     = "STANDARD"
 }
