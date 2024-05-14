8d32084ff6b6fbdb14e3676987a92dbe4c2df2e0
tranthe170 <2121013745@sv.ufm.edu.vn>
2024-05-14 11:01:04 +0700

    streaming test

---
+++ kafka/test_connection/produce_taxi_json.py
@@ -0,0 +1,31 @@
+import csv
+from datetime import datetime
+from json import dumps
+from time import sleep
+
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         key_serializer=lambda x: dumps(x).encode('utf-8'),
+                         value_serializer=lambda x: dumps(x, default=str).encode('utf-8'))
+
+file = open('data/rides.csv')
+
+csvreader = csv.reader(file)
+header = next(csvreader)
+for row in csvreader:
+    key = {"vendorId": int(row[0])}
+
+    value = {"vendorId": int(row[0]),
+            "passenger_count": int(row[3]),
+            "trip_distance": float(row[4]),
+            "pickup_location": int(row[7]),
+            "dropoff_location": int(row[8]),
+            "payment_type": int(row[9]),
+            "total_amount": float(row[16]),
+            "pickup_datetime": datetime.now()
+            }
+
+    producer.send('yellow_taxi_ride.json', value=value, key=key)
+    print("producing")
+    sleep(1)
