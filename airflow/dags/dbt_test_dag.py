90eaf10635442dea365dd69487df1762d0ebe580
tranthe170 <2121013745@sv.ufm.edu.vn>
2024-05-14 23:11:09 +0700

    remove extra lines

--- airflow/dags/dbt_test_dag.py
+++ airflow/dags/dbt_test_dag.py
@@ -20,7 +20,7 @@ with DAG(
 
     dbt_test_task = BashOperator(
         task_id = "dbt_test",
-        bash_command = "cd /dbt && dbt run"
+        bash_command = "cd /dbt && dbt compile --profiles-dir ."
     )
 
     dbt_test_task
