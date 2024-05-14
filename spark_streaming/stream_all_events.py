e92a70fe12e79845b0a5e3ef640e00f23925a8be
tranthe170 <2121013745@sv.ufm.edu.vn>
2024-05-14 20:49:12 +0700

    await any termination

--- spark_streaming/stream_all_events.py
+++ spark_streaming/stream_all_events.py
@@ -2,7 +2,6 @@
 # spark-submit \
 #   --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.3 \
 # stream_all_events.py
-
 import os
 
 from schema import (auth_events_schema, listen_events_schema,
@@ -10,43 +9,48 @@ from schema import (auth_events_schema, listen_events_schema,
 from streaming_functions import *
 
 KAFKA_ADDRESS = os.getenv("KAFKA_ADDRESS", 'localhost')
-
 KAFKA_PORT = "9092"
 # will raise error if env variable not found
 GCS_STORAGE_PATH = os.environ["GCS_STORAGE_PATH"]
 LISTEN_EVENTS_TOPIC = "listen_events"
 PAGE_VIEW_EVENTS_TOPIC = "page_view_events"
 AUTH_EVENTS_TOPIC = "auth_events"
+AWAIT_TERMINATION_TIMEOUT = 60
 
 # initialize a spark session
 spark = create_or_get_spark_session('Eventsim Stream')
-
+spark.streams.resetTerminated()
 # listen events stream
 listen_events = create_kafka_read_stream(
     spark, KAFKA_ADDRESS, KAFKA_PORT, LISTEN_EVENTS_TOPIC)
 listen_events = process_stream(listen_events, listen_events_schema)
-
-
 # page view stream
 page_view_events = create_kafka_read_stream(
     spark, KAFKA_ADDRESS, KAFKA_PORT, PAGE_VIEW_EVENTS_TOPIC)
 page_view_events = process_stream(page_view_events, page_view_events_schema)
-
 # auth stream
 auth_events = create_kafka_read_stream(
     spark, KAFKA_ADDRESS, KAFKA_PORT, AUTH_EVENTS_TOPIC)
 auth_events = process_stream(auth_events, auth_events_schema)
-
 # write a file to storage every 2 minutes in parquet format
 listen_events_writer = create_file_write_stream(listen_events,
                                                 f"{GCS_STORAGE_PATH}/{LISTEN_EVENTS_TOPIC}",
-                                                f"{GCS_STORAGE_PATH}/checkpoint/{LISTEN_EVENTS_TOPIC}")
+                                                f"{GCS_STORAGE_PATH}/checkpoint/{LISTEN_EVENTS_TOPIC}"
+                                                )
+
 page_view_events_writer = create_file_write_stream(page_view_events,
                                                    f"{GCS_STORAGE_PATH}/{PAGE_VIEW_EVENTS_TOPIC}",
-                                                   f"{GCS_STORAGE_PATH}/checkpoint/{PAGE_VIEW_EVENTS_TOPIC}")
+                                                   f"{GCS_STORAGE_PATH}/checkpoint/{PAGE_VIEW_EVENTS_TOPIC}"
+                                                   )
+
 auth_events_writer = create_file_write_stream(auth_events,
                                               f"{GCS_STORAGE_PATH}/{AUTH_EVENTS_TOPIC}",
-                                              f"{GCS_STORAGE_PATH}/checkpoint/{AUTH_EVENTS_TOPIC}")
+                                              f"{GCS_STORAGE_PATH}/checkpoint/{AUTH_EVENTS_TOPIC}"
+                                              )
+
+
 listen_events_writer.start()
+auth_events_writer.start()
 page_view_events_writer.start()
-auth_events_writer.start().awaitTermination()
+
+spark.streams.awaitAnyTermination()
