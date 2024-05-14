824aa891ee9506c8d6818f44174bed01b5177f19
tranthe170 <2121013745@sv.ufm.edu.vn>
2024-05-14 20:58:30 +0700

    multinode cluster spark

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,29 +56,36 @@ resource "google_storage_bucket" "bucket" {
   }
 }
 
-resource "google_dataproc_cluster" "spark_cluster" {
-  name   = "streamify-spark-cluster"
+resource "google_dataproc_cluster" "mulitnode_spark_cluster" {
+  name   = "streamify-multinode-spark-cluster"
   region = var.region
 
   cluster_config {
 
+    staging_bucket = "streamify"
+
     gce_cluster_config {
       network = var.network
       zone    = var.zone
-
       shielded_instance_config {
         enable_secure_boot = true
       }
     }
 
-    staging_bucket = "streamify"
-
     master_config {
       num_instances = 1
       machine_type  = "e2-standard-2"
       disk_config {
-        boot_disk_type    = "pd-standard"
-        boot_disk_size_gb = 40
+        boot_disk_type    = "pd-ssd"
+        boot_disk_size_gb = 30
+      }
+    }
+
+    worker_config {
+      num_instances = 2
+      machine_type  = "e2-medium"
+      disk_config {
+        boot_disk_size_gb = 30
       }
     }
 
