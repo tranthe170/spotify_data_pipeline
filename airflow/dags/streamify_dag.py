c483ed0e3af02e022ea0262504e199ed0ae58e28
tranthe170 <2121013745@sv.ufm.edu.vn>
2024-05-16 11:29:52 +0700

    state codes and better queries

--- airflow/dags/streamify_dag.py
+++ airflow/dags/streamify_dag.py
@@ -10,36 +10,23 @@ from airflow import DAG
 
 EVENTS = ['listen_events', 'page_view_events', 'auth_events'] # we have data coming in from three events
 
-#Make Dynamic
-# BIG_QUERY_TABLE_NAME = 'listen_events'
-# INSERT_QUERY = "{% include 'sql/listen_events.sql' %}"
-# EXTERNAL_TABLE_NAME = f'{BIG_QUERY_TABLE_NAME}_{EXECUTION_DATETIME_STR}'
-# EVENTS_PATH = f'{BIG_QUERY_TABLE_NAME}/month={EXECUTION_MONTH}/day={EXECUTION_DAY}/hour={EXECUTION_HOUR}'
-# EVENTS_SCHEMA = schema[BIG_QUERY_TABLE_NAME]
-###
 
 GCP_PROJECT_ID = os.environ.get('GCP_PROJECT_ID')
 GCP_GCS_BUCKET = os.environ.get('GCP_GCS_BUCKET')
 BIGQUERY_DATASET = os.environ.get('BIGQUERY_DATASET', 'streamify_stg')
-
 EXECUTION_MONTH = '{{ logical_date.strftime("%-m") }}'
 EXECUTION_DAY = '{{ logical_date.strftime("%-d") }}'
 EXECUTION_HOUR = '{{ logical_date.strftime("%-H") }}'
 EXECUTION_DATETIME_STR = '{{ logical_date.strftime("%m%d%H") }}'
-
 TABLE_MAP = { f"{event.upper()}_TABLE" : event for event in EVENTS}
-
 MACRO_VARS = {"GCP_PROJECT_ID":GCP_PROJECT_ID, 
               "BIGQUERY_DATASET": BIGQUERY_DATASET, 
               "EXECUTION_DATETIME_STR": EXECUTION_DATETIME_STR
               }
-
 MACRO_VARS.update(TABLE_MAP)
-
 default_args = {
     'owner' : 'airflow'
 }
-
 with DAG(
     dag_id = f'streamify_dag',
     default_args = default_args,
@@ -54,46 +41,43 @@ with DAG(
 
     initate_dbt_task = BashOperator(
         task_id = 'dbt_initiate',
-        bash_command = 'cd /dbt && dbt deps && dbt compile --profiles-dir .'
+        bash_command = 'cd /dbt && dbt deps && dbt seed --select state_codes --profiles-dir . --target prod'
     )
 
     execute_dbt_task = BashOperator(
         task_id = 'dbt_streamify_run',
-        bash_command = 'cd /dbt && dbt deps && dbt run --profiles-dir .'
+        bash_command = 'cd /dbt && dbt deps && dbt run --profiles-dir . --target prod'
     )
 
     for event in EVENTS:
-
+        
         staging_table_name = event
         insert_query = f"{{% include 'sql/{event}.sql' %}}" #extra {} for f-strings escape
         external_table_name = f'{staging_table_name}_{EXECUTION_DATETIME_STR}'
         events_data_path = f'{staging_table_name}/month={EXECUTION_MONTH}/day={EXECUTION_DAY}/hour={EXECUTION_HOUR}'
         events_schema = schema[event]
-
         create_external_table_task = create_external_table(event,
                                                            GCP_PROJECT_ID, 
                                                            BIGQUERY_DATASET, 
                                                            external_table_name, 
                                                            GCP_GCS_BUCKET, 
                                                            events_data_path)
-
         create_empty_table_task = create_empty_table(event,
                                                      GCP_PROJECT_ID,
                                                      BIGQUERY_DATASET,
                                                      staging_table_name,
                                                      events_schema)
-
+                                                
         execute_insert_query_task = insert_job(event,
                                                insert_query,
                                                BIGQUERY_DATASET,
                                                GCP_PROJECT_ID)
-
         delete_external_table_task = delete_external_table(event,
                                                            GCP_PROJECT_ID, 
                                                            BIGQUERY_DATASET, 
                                                            external_table_name)
-
-
+                    
+        
         create_external_table_task >> \
         create_empty_table_task >> \
         execute_insert_query_task >> \
--- dbt/models/core/fact_streams.sql
+++ dbt/models/core/fact_streams.sql
@@ -1,20 +1,25 @@
-{{ config(materialized = 'table') }}
+{{ config(
+  materialized = 'table',
+  partition_by={
+    "field": "ts",
+    "data_type": "timestamp",
+    "granularity": "hour"
+  }
+  ) }}
 
 SELECT 
     dim_users.userKey AS userKey,
-    dim_artists.artistKey AS artistKey,
-    dim_songs.songKey AS songKey ,
-    dim_datetime.dateKey AS dateKey,
-    dim_location.locationKey AS locationKey
-    listen_events.ts
- FROM source('staging', 'listen_events')
-  JOIN {{ ref('dim_users') }} 
-    ON listen_events.user_id = dim_users.user_id AND dim_users.current_row = 1
-  JOIN {{ ref('dim_artists') }} 
-    ON listen_events.artist_name = dim_artists.artist_name
-  JOIN {{ ref('dim_songs') }} 
-    ON listen_events.artist_name = dim_songs.artist_name AND listen_events.song = dim_songs.title
-  JOIN {{ ref('dim_location') }} 
-    ON listen_events.city = dim_location.city AND listen_events.state = dim_location.state AND listen_events.lat = dim_location.latitude AND listen_events.lon = dim_location.longitude 
-  JOIN {{ ref('dim_datetime') }} 
-    ON dim_datetime.date = date_trunc('hour', listen_events.ts)
+	@@ -8,13 +15,13 @@ SELECT
+    dim_location.locationKey AS locationKey,
+    listen_events.ts AS ts
+ FROM {{ source('staging', 'listen_events') }}
+  LEFT JOIN {{ ref('dim_users') }} 
+    ON listen_events.userId = dim_users.userId AND CAST(listen_events.ts AS DATE) >= dim_users.rowActivationDate AND CAST(listen_events.ts AS DATE) < dim_users.RowExpirationDate
+  LEFT JOIN {{ ref('dim_artists') }} 
+    ON listen_events.artist = dim_artists.name
+  LEFT JOIN {{ ref('dim_songs') }} 
+    ON listen_events.artist = dim_songs.artistName AND listen_events.song = dim_songs.title
+  LEFT JOIN {{ ref('dim_location') }} 
+    ON listen_events.city = dim_location.city AND listen_events.state = dim_location.stateCode AND listen_events.lat = dim_location.latitude AND listen_events.lon = dim_location.longitude 
+  LEFT JOIN {{ ref('dim_datetime') }} 
+    ON dim_datetime.date = date_trunc(listen_events.ts, HOUR)
